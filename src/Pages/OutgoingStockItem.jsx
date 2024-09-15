33028c1efc8961e29acfdc00fc5b9ec2a7be7d03
eslam_alhendawy <eslam_alhendawy@outlook.com>
2024-09-15 18:15:10 +0300

    Allow storage_employee to return storage items

--- src/Pages/OutgoingStockItem.jsx
+++ src/Pages/OutgoingStockItem.jsx
@@ -20,7 +20,6 @@ const OutgoingStockItem = () => {
   useEffect(() => {
     const fetchItem = async () => {
       const response = await getData(`sells/${id}`, localStorage.getItem("token"));
-      console.log(response);
       if (response.data) {
         setItem(response.data);
         setLoading(false);
@@ -41,7 +40,7 @@ const OutgoingStockItem = () => {
   };
 
   const handleReturn = async () => {
-    if (userData.role !== "admin") {
+    if (userData.role == "manager" || userData.role == "bill_employee") {
       return toast.error("غير مسموح لك بالحذف");
     }
     const response = await postData(`return`, { user: userData.id, sell: item._id, o_wieght: item.o_wieght, size_o: item.size_o, product_code: item.product_code, priceForKilo: item.priceForKilo, price_allQuantity: item.price_allQuantity, refund_amount: item.pay_now, clint: item.clint._id, product: item.product._id }, localStorage.getItem("token"));
